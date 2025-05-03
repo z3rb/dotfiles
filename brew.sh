cad92956f9049ee31f64cf402498481d0f1f4162
Zachary <zachary@Zacharys-MBP.lan>
2025-05-03 19:20:14 -0400

    Write brew install script

---
+++ brew.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Install packages with Homebrew
+brew install awscli
+brew install bat
+brew install docker
+brew install eza
+brew install fzf
+brew install gh
+brew install git
+brew install jless
+brew install jq
+brew install ncdu
+brew install newman
+brew install ripgrep
+brew install ruff
+brew install shellcheck
+brew install starship
+brew install stow
+brew install tealdeer
+brew install terraform-docs
+brew install tflint
+brew install uv
+brew install xh
+brew install yazi
+brew install zip
+brew install zoxide
