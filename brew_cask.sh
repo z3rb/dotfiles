dc6833a0533d050df0e87c9b88aa8895ee1d6bc2
Zach <47464859+z3rb@users.noreply.github.com>
2025-05-03 19:22:53 -0400

    Write scripts for brew installations

---
+++ brew_cask.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Install graphical applications with Homebrew
+brew install --cask 1password
+brew install --cask aws-vault
+brew install --cask font-source-sans-3
+brew install --cask font-sf-mono-nerd-font-ligaturized
+brew install --cask google-chrome
+brew install --cask obsidian
+brew install --cask orbstack
+brew install --cask postman
+brew install --cask proton-drive
+brew install --cask protonvpn
+brew install --cask slack
+brew install --cask spotify
+brew install --cask zed
