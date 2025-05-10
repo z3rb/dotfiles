922a08bd81662d962a7c031302607f5965cd8d37
Zachary <zachary@Zacharys-MBP.lan>
2025-05-10 18:17:16 -0400

    Update zsh alias names for ls

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,6 +3,6 @@ alias brewup="brew update && brew upgrade && brew cleanup"
 alias cat="bat"
 alias log="git log --decorate --graph --oneline"
 alias logs="git log --decorate --graph --oneline --stat"
-alias lom='eza -ahlo --time-style="+%Y-%m-%d %H:%M"'
-alias lomt='eza -ahloT --time-style="+%Y-%m-%d %H:%M"'
+alias ls='eza -ahlo --time-style="+%Y-%m-%d %H:%M"'
+alias lstree='eza -ahloT --time-style="+%Y-%m-%d %H:%M"'
 alias reload="source ~/.zshrc"
