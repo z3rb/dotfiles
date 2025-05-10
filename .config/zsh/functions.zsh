5f70513d41a40d24c7e4f4ab443f4be111c01bb6
Zachary <zachary@Zacharys-MBP.lan>
2025-05-10 19:51:40 -0400

    Define zsh function to compare lines between two sorted files

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,5 +1,18 @@
-#!/bin/zsh
+#!/bin/bash
 # Navigate to new directory
 mkcd() {
-    mkdir -p "$1" && cd "$1"
+    mkdir -p "$1"
+    cd "$1" || exit
+}
+
+# Compare lines between two sorted files
+venn() {
+    echo "> Lines common to $1 and $2:"
+    comm -12 "$1" "$2"
+
+    echo "> Lines unique to $1:"
+    comm -23 "$1" "$2"
+
+    echo "> Lines unique to $2:"
+    comm -13 "$1" "$2"
 }
