6b839c8297e7ba30d9f042b5ce2a4c82c3ed4786
Zachary <zachary@Zacharys-MBP.lan>
2025-05-10 12:03:19 -0400

    Set zsh aliases for git logs

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,4 +1,6 @@
 alias cat="bat"
+alias log="git log --decorate --graph --oneline"
+alias logs="git log --decorate --graph --oneline --stat"
 alias lom='eza -ahlo --time-style="+%Y-%m-%d %H:%M"'
 alias lomt='eza -ahloT --time-style="+%Y-%m-%d %H:%M"'
 alias reload="source ~/.zshrc"
