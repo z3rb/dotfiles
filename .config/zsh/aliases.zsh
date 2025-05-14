24f6005e6449f60b42d0a309c6894082f92b6345
Zachary <zachary@Zacharys-MBP.lan>
2025-05-13 21:34:23 -0400

    Add git aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,8 +1,6 @@
 alias brewtree="brew list | xargs brew deps --tree"
 alias brewup="brew update && brew upgrade && brew cleanup"
 alias cat="bat"
-alias log="git log --decorate --graph --oneline"
-alias logstat="git log --decorate --graph --oneline --stat"
 alias ls='eza -ahlo --time-style="+%Y-%m-%d %H:%M"'
 alias lstree='eza -ahloT --time-style="+%Y-%m-%d %H:%M"'
 alias reload="source ~/.zshrc"
@@ -12,3 +10,9 @@ alias tfls="terraform state list"
 alias tfplan="terraform plan"
 alias tfshow="terraform show -json"
 alias tfval="terraform validate"
+
+# Git
+alias gbr="git branch -v"
+alias glog="git log --decorate --graph --oneline"
+alias glogs="git log --decorate --graph --oneline --stat"
+alias gst="git status -s"
