7da3db2e162fcde0d87ad628716dc20d7d4c4f8a
Zachary <zachary@Zacharys-MBP.lan>
2025-05-13 21:41:30 -0400

    Organize other aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,8 +1,3 @@
-alias cat="bat"
-alias ls='eza -ahlo --time-style="+%Y-%m-%d %H:%M"'
-alias lstree='eza -ahloT --time-style="+%Y-%m-%d %H:%M"'
-alias reload="source ~/.zshrc"
-
 # Git
 alias gbr="git branch -v"
 alias glog="git log --decorate --graph --oneline"
@@ -20,3 +15,9 @@ alias tfls="terraform state list"
 alias tfplan="terraform plan"
 alias tfshow="terraform show -json"
 alias tfval="terraform validate"
+
+# Other
+alias cat="bat"
+alias ls='eza -ahlo --time-style="+%Y-%m-%d %H:%M"'
+alias reload="source ~/.zshrc"
+alias tree='eza -ahloT --time-style="+%Y-%m-%d %H:%M"'
