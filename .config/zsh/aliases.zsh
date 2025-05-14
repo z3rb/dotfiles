e680042fab41777630b5a0e2d10839863f2319ed
Zachary <zachary@Zacharys-MBP.lan>
2025-05-13 21:36:21 -0400

    Organize tf aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,15 +4,17 @@ alias cat="bat"
 alias ls='eza -ahlo --time-style="+%Y-%m-%d %H:%M"'
 alias lstree='eza -ahloT --time-style="+%Y-%m-%d %H:%M"'
 alias reload="source ~/.zshrc"
-alias tffmt="terraform fmt -recursive"
-alias tfinit="terraform init"
-alias tfls="terraform state list"
-alias tfplan="terraform plan"
-alias tfshow="terraform show -json"
-alias tfval="terraform validate"
 
 # Git
 alias gbr="git branch -v"
 alias glog="git log --decorate --graph --oneline"
 alias glogs="git log --decorate --graph --oneline --stat"
 alias gst="git status -s"
+
+# Terraform
+alias tffmt="terraform fmt -recursive"
+alias tfinit="terraform init"
+alias tfls="terraform state list"
+alias tfplan="terraform plan"
+alias tfshow="terraform show -json"
+alias tfval="terraform validate"
