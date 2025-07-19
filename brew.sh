68cdd5471e9f644a49d8592698edf81e6e526a98
Zach <47464859+z3rb@users.noreply.github.com>
2025-07-19 11:42:06 -0400

    Create brew install script

---
+++ brew.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+if ! command -v brew &>/dev/null; then
+    echo "> Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
