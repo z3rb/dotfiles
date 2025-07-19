5182d4943cd387d120e30ecc2735386619d12444
z3rb <zrbutler1@gmail.com>
2025-07-19 17:45:19 -0400

    Install git

--- brew.sh
+++ brew.sh
@@ -3,3 +3,7 @@ if ! command -v brew &>/dev/null; then
     echo "> Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+echo "> Installing Homebrew formulae..."
+brew install git
+
