ed7a2c59eeb0880a24c156950a0d4b206c5bc5e2
z3rb <zrbutler1@gmail.com>
2025-07-30 22:30:24 -0400

    Install raycast

--- brew.sh
+++ brew.sh
@@ -1,11 +1,18 @@
-#!/bin/zsh
+#!/bin/sh
 if ! command -v brew &>/dev/null; then
     echo "> Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "> Installing Homebrew formulae..."
-brew install bat eza git starship stow zoxide zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting
+formulae=(bat eza git starship stow zoxide zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting)
+for formula in "${formulae[@]}"; do
+  brew install "$formula"
+done
 
 echo "> Installing Homebrew casks..."
-brew install --cask font-fira-code-nerd-font wezterm@nightly
+casks=(font-fira-code-nerd-font raycast wezterm@nightly)
+for cask in "${casks[@]}"; do
+  brew install --cask "$cask"
+done
+
