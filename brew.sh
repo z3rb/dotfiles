f1687324d253b2316b093d15d70385aeefcc2b23
Zach <47464859+z3rb@users.noreply.github.com>
2025-08-03 21:38:55 -0400

    Install 1password

--- brew.sh
+++ brew.sh
@@ -5,13 +5,13 @@ if ! command -v brew &>/dev/null; then
 fi
 
 echo "> Installing Homebrew formulae..."
-formulae=(bat eza git starship stow zoxide zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting)
+formulae=(1password-cli bat eza git starship stow zoxide zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting)
 for formula in "${formulae[@]}"; do
   brew install "$formula"
 done
 
 echo "> Installing Homebrew casks..."
-casks=(font-fira-code-nerd-font raycast wezterm@nightly)
+casks=(1password font-fira-code-nerd-font raycast wezterm@nightly)
 for cask in "${casks[@]}"; do
   brew install --cask "$cask"
 done
