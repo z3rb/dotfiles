b2c589ff9e3c7f59b9a749c28b1f2850322edaf3
z3rb <zrbutler1@gmail.com>
2026-06-19 13:56:25 -0400

    Move history to new file

---
+++ zsh/.config/zsh/history.zsh
@@ -0,0 +1,11 @@
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+setopt appendhistory
+setopt extendedhistory
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt hist_verify
+setopt share_history
